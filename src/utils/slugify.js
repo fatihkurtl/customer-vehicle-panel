d52dcb79754b27ae4f9f9150e0746e8f6788873e
fatihkurtl <kurtf061@gmail.com>
2024-09-18 04:06:05 +0300

    utils/slugify usage explained

--- src/utils/slugify.js
+++ src/utils/slugify.js
@@ -1,16 +1,26 @@
 // https://fatihkurt.web.tr/blog/nextjs-ve-slugify-kullanm-urlleri-seo-dostu-hale-getirin/
 
 export function slugify(text) {
-    const turkishChars = {
-        'ı': 'i', 'ğ': 'g', 'ü': 'u', 'ş': 's', 'ö': 'o', 'ç': 'c',
-        'İ': 'I', 'Ğ': 'G', 'Ü': 'U', 'Ş': 'S', 'Ö': 'O', 'Ç': 'C'
-    };
+  const turkishChars = {
+    ı: "i",
+    ğ: "g",
+    ü: "u",
+    ş: "s",
+    ö: "o",
+    ç: "c",
+    İ: "I",
+    Ğ: "G",
+    Ü: "U",
+    Ş: "S",
+    Ö: "O",
+    Ç: "C",
+  };
 
-    return text
-        .toLowerCase() // Tüm karakterleri küçük harfe çevirir
-        .replace(/[^a-z0-9\s]/g, (char) => turkishChars[char] || '') // Türkçe karakterleri dönüştürür
-        .trim() // İlk ve son boslukları kaldırır
-        .replace(/\s+/g, '-') // Boşlukları "-" ile değiştir
-        .replace(/-+/g, '-') // Birden fazla "-" işaretini tek "-" yapar
-        .replace(/^-+|-+$/g, ''); // Baş veya sondaki "-" işaretlerini temizler
+  return text
+    .toLowerCase() // Tüm karakterleri küçük harfe çevirir
+    .replace(/[^a-z0-9\s]/g, (char) => turkishChars[char] || "") // Türkçe karakterleri dönüştürür
+    .trim() // İlk ve son boslukları kaldırır
+    .replace(/\s+/g, "-") // Boşlukları "-" ile değiştir
+    .replace(/-+/g, "-") // Birden fazla "-" işaretini tek "-" yapar
+    .replace(/^-+|-+$/g, ""); // Baş veya sondaki "-" işaretlerini temizler
 }
